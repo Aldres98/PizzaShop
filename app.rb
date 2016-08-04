4f186d94259239fce6de1887ecce9c32029e47a3
Aldres98 <aldres98@gmail.com>
2016-08-04 23:29:28 +0400

    adding new pizza page + main page pizza show

--- app.rb
+++ app.rb
@@ -9,10 +9,30 @@ set :database, "sqlite3:pizzashop.db"
 class Product < ActiveRecord::Base
 end
 
+before do 
+ @products = Product.all
+end
+
 get '/' do
 	erb :index
 end
 
 get '/about' do
 	erb :about
+end
+
+get '/new' do
+	@new_pizza = Product.new 
+	erb :new
+	
+end
+
+post '/new' do
+	@new_pizza = Product.new params[:product]
+	if @new_pizza.save
+		erb "<h2>New pizza succesfully added!</h2>"
+	else
+		@error = @new_pizze.full_messages.first
+		erb :new
+	end
 end
