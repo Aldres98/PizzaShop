0da3308f2dd2ced8dd189d993b0ae709673525dc
Aldres98 <aldres98@gmail.com>
2016-08-07 23:10:10 +0400

    Add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -1,13 +1,13 @@
 function something()
 {
 
-	var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
+    var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
 
-	x = x * 1 + 1;
+    x = x * 1 + 1;
 
-	window.localStorage.setItem('bbb', x); // hh['bbb'] = x 
+    window.localStorage.setItem('bbb', x); // hh['bbb'] = x 
 
-	alert(x);
+    alert(x);
 }
 
 function add_to_cart(id)
@@ -18,11 +18,30 @@ function add_to_cart(id)
     x = x * 1 + 1;
     window.localStorage.setItem(key, x);
 
-    for (var i=0, len=localStorage.length; i<len; i++ )
+    // Вывод количества товаров в корзине (алертом)
+    alert('Items in your cart: ' + cart_get_number_of_items())
+
+}
+
+function cart_get_number_of_items()
+{
+    var cnt = 0
+
+    for(var i = 0; i < window.localStorage.length; i++)
     {
-        var show_key = localStorage.key(i);
-        var show_value = localStorage[show_key];
-        console.log(show_key + "=> " + show_value);
+        var key = window.localStorage.key(i); // получаем ключ
+        var value = window.localStorage.getItem(key); // Получаем значение по ключу
+
+        if(key.indexOf('product_') == 0)
+
+        {
+
+            cnt = cnt + value * 1;
+
+        }
+
     }
+
+    return cnt;
 
 }
